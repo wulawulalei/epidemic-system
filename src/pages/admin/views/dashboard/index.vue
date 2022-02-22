b768cdcc96af9b7f5268ca6d19b8a107c797037a
wulawulalei <806913282@qq.com>
2022-02-22 22:45:54 +0800

    后台首页部分样式

--- src/pages/admin/views/dashboard/index.vue
+++ src/pages/admin/views/dashboard/index.vue
@@ -1,17 +1,282 @@
 <template>
-  <div id="dashboard" class="body-common">dashboard</div>
+  <div id="dashboard" class="body-common">
+    <div class="village">
+      <div class="item">
+        <h5 class="title">hhhh</h5>
+        <h3 class="number">789789</h3>
+      </div>
+      <div class="item">
+        <h5 class="title">hhhh</h5>
+        <h3 class="number">789789</h3>
+      </div>
+      <div class="item">
+        <h5 class="title">hhhh</h5>
+        <h3 class="number">789789</h3>
+      </div>
+      <div class="item">
+        <h5 class="title">hhhh</h5>
+        <h3 class="number">789789</h3>
+      </div>
+    </div>
+    <div class="row">
+      <div class="chart" ref="chart" style="width:calc(100% - 330px);height:430px"></div>
+      <div class="performance"></div>
+    </div>
+  </div>
 </template>
 <script>
-// import axios from 'axios'
+import Echarts from 'echarts'
 import { epidemic } from '@/api/question'
 export default {
   name: 'dashboard',
-  mounted () {
-    epidemic().then(res => {
-      console.log(res)
-    })
+  data() {
+    return {
+      epidList: [
+        {
+          title: '境外输入',
+          num: 0,
+          added: 0,
+          color: '#ffa352'
+        },
+        {
+          title: '无症状感染者',
+          num: 0,
+          added: 0,
+          color: '#791618'
+        },
+        {
+          title: '现有确诊',
+          num: 0,
+          added: 0,
+          color: '#e44a3d'
+        },
+        {
+          title: '累计确诊',
+          num: 0,
+          added: 0,
+          color: '#a31d13'
+        },
+        {
+          title: '累计死亡',
+          num: 0,
+          added: 0,
+          color: '#333'
+        },
+        {
+          title: '累计治愈',
+          num: 0,
+          added: 0,
+          color: '#34aa70'
+        }
+      ]
+    }
+  },
+  mounted() {
+    this.getEpidMessage()
+  },
+  computed: {
+    options() {
+      const xData = this.epidList.map(item => {
+        return item.title
+      })
+      const yData1 = this.epidList.map(item => {
+        return item.num
+      })
+      const yData2 = this.epidList.map(item => {
+        return item.added
+      })
+      let option = {
+        title: {
+          text: '国内疫情数据',
+          left: '0',
+          textStyle: {
+            color: '#333333',
+            fontSize: 16
+          }
+        },
+        xAxis: {
+          position: 'bottom',
+          offset: 0,
+          name: '类型',
+          //坐标轴
+          axisLine: {
+            show: false
+          },
+          data: xData
+        },
+        yAxis: {
+          offset: 0,
+          name: '人数',
+          //坐标轴
+          axisLine: {
+            show: false
+          },
+          //坐标轴的刻度
+          axisTick: {
+            show: false
+          }
+        },
+        //echarts的位置
+        grid: {
+          width: '85%',
+          height: '80%',
+          top: '20%',
+          left: '5%',
+          containLabel: true
+        },
+        series: [
+          {
+            data: yData1,
+            type: 'bar',
+            smooth: true,
+            symbolSize: 5,
+            itemStyle: {
+              normal: {
+                color: '#3195ff',
+                lineStyle: {
+                  color: '#3195ff' //折线颜色
+                }
+              }
+            },
+            textStyle: {
+              fontSize: 14,
+              color: '#999999'
+            },
+            label: {
+              show: true,
+              position: 'top',
+              color: '#999999'
+            },
+            barWidth: '20%',
+            barCategoryGap: '10%'
+          },
+          {
+            data: yData2,
+            type: 'bar',
+            smooth: true,
+            symbolSize: 5,
+            itemStyle: {
+              normal: {
+                color: '#3195ff',
+                lineStyle: {
+                  color: '#3195ff' //折线颜色
+                }
+              }
+            },
+            textStyle: {
+              fontSize: 14,
+              color: '#999999'
+            },
+            label: {
+              show: true,
+              position: 'top',
+              color: '#999999'
+            },
+            barWidth: '20%',
+            barCategoryGap: '10%'
+          }
+        ],
+        tooltip: {
+          backgroundColor: '#FFFFFF',
+          textStyle: {
+            color: '#666666' //设置文字颜色
+          },
+          extraCssText: 'box-shadow:0px 3px 6px rgba(49, 149, 255, 0.16)',
+          formatter: function (params) {
+            const name = params.seriesIndex == 0 ? params.name : '较昨日'
+            const value = params.seriesIndex == 0 ? params.value : params.value > 0 ? '+' + params.value : params.value
+            return `${name}：${value}人`
+          }
+        }
+      }
+      return option
+    }
+  },
+  methods: {
+    // 获取疫情信息
+    getEpidMessage() {
+      epidemic().then((res) => {
+        const input = res.data.chinaTotal.total.input
+        const noSymptom = res.data.chinaTotal.extData.noSymptom
+        const confirm = res.data.chinaTotal.total.confirm
+        const dead = res.data.chinaTotal.total.dead
+        const heal = res.data.chinaTotal.total.heal
+        const now = confirm - dead - heal
+        const inputAdded = res.data.chinaTotal.today.input
+        const noSymptomAdded = res.data.chinaTotal.extData.incrNoSymptom
+        const confirmAdded = res.data.chinaTotal.today.confirm
+        const deadAdded = res.data.chinaTotal.today.dead
+        const healAdded = res.data.chinaTotal.today.heal
+        const nowAdded = res.data.chinaTotal.today.storeConfirm
+        const list = [
+          {
+            num: input,
+            added: inputAdded
+          },
+          {
+            num: noSymptom,
+            added: noSymptomAdded
+          },
+          {
+            num: now,
+            added: nowAdded
+          },
+          {
+            num: confirm,
+            added: confirmAdded
+          },
+          {
+            num: dead,
+            added: deadAdded
+          },
+          {
+            num: heal,
+            added: healAdded
+          }
+        ]
+        this.epidList = this.epidList.map((item, index) => {
+          item = { ...item, ...list[index] }
+          return item
+        })
+        this.initEcharts()
+      })
+    },
+    //曲线图初始化
+    initEcharts() {
+      let myChart = Echarts.init(this.$refs.chart)
+      myChart.setOption(this.options)
+    },
   }
 }
 </script>
 <style lang='scss' scoped>
+#dashboard {
+  padding-top: 20px;
+  background-color: #fafbfe;
+  .village {
+    display: flex;
+    flex-wrap: wrap;
+    .item {
+      flex: 1;
+      min-width: 230px;
+      padding: 24px;
+      margin: 0 15px 30px 15px;
+      box-shadow: 0 0 35px 0 rgba(154, 161, 171, 0.15);
+      transition: transform 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
+      .number {
+        margin: 24px 0;
+      }
+    }
+  }
+  .row {
+    display: flex;
+    width: 100%;
+    .chart {
+      margin-left: 30px;
+    }
+    .performance {
+      width: 300px;
+    }
+  }
+}
 </style>
--- src/pages/admin/views/notice/index.vue
+++ src/pages/admin/views/notice/index.vue
@@ -1,10 +1,140 @@
 <template>
-  <div id="notice" class="body-common">notice</div>
+  <div id="notice" class="body-common">
+    <div class="remind">
+      <h4>小区公告</h4>
+      <p>该页面为小区公告，管理员发布公告</p>
+      <p>
+        添加一个公告，
+        <span class="add">点击这里进行添加</span>
+      </p>
+    </div>
+    <div class="header">
+      <div class="pagination">
+        <el-pagination
+          :total="pagiantion.total"
+          :page-size="pagiantion.limit"
+          :page-sizes="[2, 4, 6]"
+          @current-change="handleCurrentChange"
+          :current-page.sync="pagiantion.page"
+          :pager-count="5"
+          layout="prev, pager, next,jumper"
+        ></el-pagination>
+      </div>
+      <div class="search">
+        <label>
+          Search:
+          <input type="text" />
+        </label>
+      </div>
+    </div>
+    <div class="contain">
+      <div class="item">
+        <img src="../../../../assets/user/dashboard.png" alt />
+        <p>hhhhhhhhhhhhhh</p>
+        <div class="info">
+          <img src="../../../../assets/user/dashboard.png" alt />
+          <div class="avatar">dfd</div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 <script>
+import { Pagination } from 'element-ui'
 export default {
-  name: 'notice'
+  name: 'notice',
+  components: {
+    [Pagination.name]: Pagination,
+  },
+  data() {
+    return {
+      pagiantion: {
+        total: 0,
+        page: 1,
+        limit: 10
+      }
+    }
+  },
+  methods: {
+    handleCurrentChange() {
+      console.log(1)
+    }
+  }
 }
 </script>
 <style lang='scss' scoped>
+#notice {
+  .remind {
+    padding: 20px 0 0;
+    h4 {
+      margin: 0;
+      margin-bottom: 10px;
+    }
+    p {
+      font-size: 14px;
+      color: #98a6ad;
+      .add {
+        color: $primary;
+        cursor: pointer;
+      }
+    }
+  }
+  .header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 20px 0;
+    input {
+      width: 180px;
+      height: 30px;
+      margin-left: 7px;
+      padding: 5px 12px;
+      border: 1px solid #ccc;
+      border-radius: 4px;
+      outline: none;
+    }
+  }
+  .contain {
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    .item {
+      width: 236px;
+      margin-bottom: 10px;
+      background-color: #fff;
+      box-shadow: 0 1px 3px rgba(0, 0, 0, 0.02), 0 4px 8px rgba(0, 0, 0, 0.02);
+      transition: transform 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
+      cursor: pointer;
+      border-radius: $radius;
+      > img {
+        width: 100%;
+        min-height: 100px;
+        border-radius: $radius $radius 0 0;
+      }
+      p {
+        padding: 0 16px;
+        margin: 10px 0;
+        word-wrap: break-word;
+      }
+      .info {
+        display: flex;
+        align-items: center;
+        margin: 16px 0;
+        padding: 0 16px;
+        color: #999;
+        img {
+          width: 34px;
+          height: 34px;
+          margin-right: 20px;
+          background-color: #fff;
+          border-radius: 50%;
+        }
+      }
+      &:hover {
+        box-shadow: 0 1px 3px rgba(0, 0, 0, 0.02),
+          0 16px 32px -4px rgba(0, 0, 0, 0.17);
+        transform: translateY(-1px);
+      }
+    }
+  }
+}
 </style>
